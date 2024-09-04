c040f9ba8adc87522cc593b4e35e1eef8e89cb55
mariam <makokirvalidze@gmail.com>
2024-09-04 17:57:30 +0400

    Adding Header

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-
 
 import Animals from './pages/Animals';
 import Categories from './pages/Categories';
 import AnimalCategories from './pages/AnimalCategories';
-// import Header from './components/Header';
-// import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
@@ -19,12 +18,12 @@ const App = () => {
           <div className="admin-content">
             <Sidebar />
             <main>
-              <Switch>
-                <Route path="/animals" component={Animals} />
-                <Route path="/categories" component={Categories} />
-                <Route path="/animal-categories" component={AnimalCategories} />
-                <Redirect from="/" to="/animals" />
-              </Switch>
+              <Routes>
+                <Route path="/animals" element={<Animals />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/animal-categories" element={<AnimalCategories />} />
+                <Route path="*" element={<Navigate to="/animals" />} />
+              </Routes>
             </main>
           </div>
         </div>
